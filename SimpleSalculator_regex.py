fce930cca15622fd976d668b8ca866faa6572f56
Sophia <sbaker@putneyschool.org>
2018-01-25 12:51:26 -0500

    Devision + Multiplication

--- SimpleSalculator_regex.py
+++ SimpleSalculator_regex.py
@@ -1,8 +1,5 @@
 import re
 
-
-print("welcome to calculator.py")
-
 #returns the summ of two numbers
 def add(num1,num2):
 	return  num1 + num2
@@ -19,45 +16,58 @@ def multiply(num1,num2):
 def divide(num1,num2):
 	return  num1 / num2
 
+
 #Arrays call on caluculatons, to calulate mutliple variables:
-def addArray(array):
-	return add(float(array[0]),float(array[1]))
+def addWhile(string):
+	return add(1,2)
 
-def subtractArray(array):
-	return subtract(float(array[0]),float(array[1]))
+def subtractWhile(string):
+	return subtract(1,2)
 
-def multiplyArray(array):
-	return multiply(float(array[0]),float(array[1]))
+def multiplyWhile(string):
+	print("Multiplication:")
+	while '*' in string:
+		firstMultiplier = re.findall("(\(?\d+\.?\d*\)?)\*",string)[0]
+		secondMultiplier = re.findall("\*(\(?\d+\.?\d*\)?)",string)[0]
 
-def divideArray(array):
-	return divide(float(array[0]),float(array[1]))
-
-
-#main() calls on calculations
-def main():
-	
-	debug = True
-
-	if debug:
-		operation = "18.2-93/2*2.5+21*5-2" #does not work for more than 1 multiplication sign but not for one either
-	else:
-		operation = input("What calculation would you like to complete:") #or raw_input ?
-
-	while '*' in operation:
-		firstMultiplier = re.findall("(\(?\d+\.?\d*\)?)\*",operation)[0]
-		secondMultiplier = re.findall("\*(\(?\d+\.?\d*\)?)",operation)[0]
-
-		print(firstMultiplier)
-		print(secondMultiplier)
+		print("var1 = " + firstMultiplier)
+		print("var2 = " + secondMultiplier)
 
 		answer = multiply(float(firstMultiplier),float(secondMultiplier))
-		print(answer)
-		operation = operation.replace(firstMultiplier + "*" + secondMultiplier, str(answer))
+		print("answer = " + str(answer))
+		string = string.replace(firstMultiplier + "*" + secondMultiplier, str(answer))
+		print("string = " + string)
 
-	return operation
+	return string
+
+def divideWhile(string):
+	print("Division:")
+	while '/' in string:
+		firstDivider = re.findall("(\(?\d+\.?\d*\)?)\/",string)[0]
+		secondDivider = re.findall("\/(\(?\d+\.?\d*\)?)",string)[0]
+
+		print("var1 = " + firstDivider)
+		print("var2 = " + secondDivider)
+
+		answer = divide(float(firstDivider),float(secondDivider))
+		print("answer = " + str(answer))
+		string = string.replace(firstDivider + "/" + secondDivider, str(answer))
+		print("string = " + string)
+
+	return string
 
 	
-	 
-#Calling on funtion main()
-print(main())
+print("welcome to calculator.py")
+
+
+debug = True
+
+if debug:
+	operation = "18.2-93/2*2.5+21*5-2"
+else:
+	operation = input("What calculation would you like to complete:")
+
+#performing calulations in order of operation:	
+operation = multiplyWhile(operation)
+operation = divideWhile(operation)
 
