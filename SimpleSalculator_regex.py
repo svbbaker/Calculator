0e076954b77e2ad271384f19848bc4910b22708b
Sophia <sbaker@putneyschool.org>
2018-01-30 10:05:35 -0500

    multiply/divide + addition/subtraction
    no bugs:)

--- SimpleSalculator_regex.py
+++ SimpleSalculator_regex.py
@@ -3,77 +3,48 @@
 #January 2018
 
 import re
-import numpy as np
-
-#returns the pruduct (*) of two numbers
-def multiply(num1,num2):
-	return  num1 * num2
-
-#returns the product (/) of two numbers
-def divide(num1,num2):
-	return  num1 / num2
-
 
 #Strings call on operations to calulate mutliple variables using Regex
 def sumAll(string):
 	print("Addition & Subtraction:")
-	for i in string:
-		if i == '+' or i == '-' in string:
-			#posList = re.findall("\+(\(?\d+\.?\d*\)?)",string)
-			#negList = re.findall("(\-\(?\d+\.?\d*\)?)", string)
-			List = re.findall("(\-?\(?\d+\.?\d*\)?)", string)
-			#print("posList: " + str(posList))
-			#print("negList: " + str(negList))
-			print("firstNum: " + str(List))
-
-			#posAnswer = sum(float(x) for x in posList)
-			#negAnswer = sum(float(x) for x in negList)
-			#print("posSum = " + str(posAnswer))
-			#print("negSum = " + str(negAnswer))
-
-			#answer1 = add(posAnswer,negAnswer)
-			answer = sum(float(x) for x in List)
-			#print(str(posAnswer) + " + " + str(negAnswer) + " = " + str(answer1))
-			print("answer = " + str(answer))# + str(answer1) + " or " + str(answer))
-			break #how do I make it only itterate once rather than having to break it
-		else:
-			print("no addition or subtraction")
-			answer = string
-			break
+	#check if string contains + or - if not return answer
+	if '+' in string or '-' in string:
+		for i in string:
+			if i == '+' in string or i == '-' in string:
+				List = re.findall("(\-?\(?\d+\.?\d*\)?)", string)
+				print("List: " + str(List))
+				answer = sum(float(x) for x in List)
+				print("answer = " + str(answer))
+				break
+	else:
+		print("no Multiplication or Division")
+		answer = string
+	
 	return answer
 
-def multiplyWhile(string):
-	print("Multiplication:")
-	while '*' in string:
-		firstMultiplier = re.findall("(\(?\d+\.?\d*\)?)\*",string)[0]
-		secondMultiplier = re.findall("\*(\(?\d+\.?\d*\)?)",string)[0]
+def mdWhile(string):
+	print("Multiplication & Division:")
+	while '*' in string or '/' in string:
+		var1 = re.findall("(\(?\d+\.?\d*\)?)[\*\/]",string)[0]
+		var2 = re.findall("[\*\/](\(?\d+\.?\d*\)?)",string)[0]
+		operator = re.findall("[\*\/]",string)[0]
 
-		print("var1 = " + firstMultiplier)
-		print("var2 = " + secondMultiplier)
+		print("var1 = " + var1)
+		print("var2 = " + var2)
+		print("operation: " + operator)
 
-		answer = multiply(float(firstMultiplier),float(secondMultiplier))
-		print("answer = " + str(answer))
-		string = string.replace(firstMultiplier + "*" + secondMultiplier, str(answer))
+		if operator == '*':
+			answer = float(var1) * float(var2)
+			print("answer = " + str(answer))
+		elif operator == '/':
+			answer = float(var1)/float(var2)
+			print("answer = " + str(answer))
+		
+		string = string.replace(var1 + operator + var2, str(answer))
+
 		print("string = " + string)
 
 	return string
-
-def divideWhile(string):
-	print("Division:")
-	while '/' in string:
-		firstDivider = re.findall("(\(?\d+\.?\d*\)?)\/",string)[0]
-		secondDivider = re.findall("\/(\(?\d+\.?\d*\)?)",string)[0]
-
-		print("var1 = " + firstDivider)
-		print("var2 = " + secondDivider)
-
-		answer = divide(float(firstDivider),float(secondDivider))
-		print("answer = " + str(answer))
-		string = string.replace(firstDivider + "/" + secondDivider, str(answer))
-		print("string = " + string)
-
-	return string
-
 
 	
 print("welcome to calculator.py")
@@ -82,18 +53,12 @@ print("welcome to calculator.py")
 debug = True
 
 if debug:
-	operation = "18.2+27/8*12/3+2*0.4+9"
+	operation = "18.2*27-3/8*12/32*0.43+13"
 else:
 	operation = input("What calculation would you like to complete:")
 
 #performing calulations in order of operation:	
-operation1 = multiplyWhile(operation)
-operation2 = divideWhile(operation1) #GET RID OF THE op.123 WHEN GET RID OF PRINTS
-operation3 = sumAll(operation2)
-print("answers:")
-print(operation1)
-print(operation2)
-print(operation3)
+operation = mdWhile(operation)
+operation = sumAll(operation)
 
-print("the answer to your calculation is: " + str(operation3)) #EACH PIECE IS CORRECT BUT NOT WHEN TOGETHER!!!!!!
-
+print("the answer to your calculation is: " + str(operation))
