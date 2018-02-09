caa73db56abdc3f7f1d47fff557b84233edc4ede
Sophia <sbaker@putneyschool.org>
2018-02-09 17:46:15 -0500

    varMult: exponents fixed
    need to work on:
    coefficients - the exp are currently being counted which the should not be

--- Calculator_x_Regex.py
+++ Calculator_x_Regex.py
@@ -12,17 +12,62 @@ def varMult(input1,input2):
 	print("Variable Multiplicaiton:")
 	exp = 0
 	coef = 1
-	if 'x' in input1:
-		exp += 1
-	if 'x' in input2:
-		exp += 1
-	if re.findall("\d+",input1):
-		num = re.findall("(\d+)",input1)
-		coef = coef * num #make into floats
-	if re.findall("\d+",input2):
-		num = re.findall("(\d+)",input2)
-		coef = coef * num
-	answer = coef, exp
+	var1 = re.findall("([a-z])",input1)[0]
+	var2 = re.findall("([a-z])",input2)[0]
+	if var1 == var2:
+		exp1 = input1.count(var1)
+		exp2 = input2.count(var2)
+		exp = exp1 + exp2
+		#if var1 in input1:
+		#	exp += 1
+		#if var2 in input2:
+		#	exp += 1
+		if re.findall("\^(\-?\d+)",input1):
+			num = re.findall("\^(\-?\d+)",input1)[0]
+			num = int(num)
+			print(num)
+			exp = exp + num -1 #-1 sothat we dont acount for an extera variable from count above
+		if re.findall("\^(\-?\d+)",input2):
+			num = re.findall("\^(\-?\d+)",input2)[0]
+			print(num)
+			num = int(num)
+			print(num)
+			exp = exp + num -1
+		if re.findall("\d+",input1):
+			num = re.findall("(\d+)",input1)[0]
+			num = int(num)
+			coef = coef * num #WONT LET ME MULTIPLY FLOATS
+		if re.findall("\d+",input2):
+			num = re.findall("(\d+)",input2)[0]
+			num = int(num)
+			coef = coef * num #WONT LET ME MULTIPLY FLOATS
+		answer = coef, var1, exp
+	else: false
+
+	return answer
+
+#variable Addition
+def varAdd(input1,input2):
+	print("Variable Multiplicaiton:")
+	exp = 0
+	coef = 1
+	var1 = re.findall("([a-z])",input1)[0]
+	var2 = re.findall("([a-z])",input2)[0]
+	if var1 == var2:
+		if var1 in input1:
+			exp += 1
+		if var1 in input2:
+			exp += 1
+		if re.findall("\d+",input1):
+			num = re.findall("(\d+)",input1)[0]
+			num = int(num)
+			coef = coef * num #WONT LET ME MULTIPLY FLOATS
+		if re.findall("\d+",input2):
+			num = re.findall("(\d+)",input2)[0]
+			num = int(num)
+			coef = coef * num #WONT LET ME MULTIPLY FLOATS
+		answer = coef, var1, exp
+	else: answer = False
 	return answer
 
 
@@ -34,8 +79,8 @@ if debug:
 else:
 	operation = input("What calculation would you like to complete:")
 
-a = '2x'
-b = '3x'
+a = '4x^-2'
+b = 'x^3'
 print(varMult(a,b))
 
 #REGEX
