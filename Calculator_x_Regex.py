7dbab24ca792bf0be52068965129949d3516611f
Sophia <sbaker@putneyschool.org>
2018-02-13 14:19:39 -0500

    variable function completed (not else)

--- Calculator_x_Regex.py
+++ Calculator_x_Regex.py
@@ -8,7 +8,7 @@ import re
 
 #variables
 #variable multiplicaiton
-def varMult(input1,input2):
+def varMul(input1,input2):
 	print("Variable Multiplicaiton:")
 	exp = 0
 	coef = 1
@@ -50,6 +50,46 @@ def varMult(input1,input2):
 		anwer = False #"False"
 	return answer
 
+#variable Division
+def varDiv(input1,input2):
+	print("Variable Division:")
+	exp = 0
+	coef = 1
+	var1 = re.findall("([a-z])",input1)[0]
+	var2 = re.findall("([a-z])",input2)[0]
+	if var1 == var2:
+		#num of variable occerences
+		exp1 = input1.count(var1)
+		exp2 = input2.count(var2)
+		#exp = exp1 - exp2
+		#exponents
+		if re.findall("\^(\-?\d+)",input1):
+			num = re.findall("\^(\-?\d+)",input1)[0]
+			num = float(num)
+			expA = exp1 + num -1 #-1 sothat we dont acount for an extera variable from count above
+		if re.findall("\^(\-?\d+)",input2):
+			num = re.findall("\^(\-?\d+)",input2)[0]
+			num = float(num)
+			expB = exp2 + num -1
+		exp = expA - expB
+		#coeffients
+		if re.findall("(?<![\^\-])[\-\d\.]+",input1):
+			num = re.findall("(?<![\^\-])[\-\d\.]+",input1)[0]
+			num = float(num)
+			coef = coef * num #WONT LET ME MULTIPLY FLOATS
+		if re.findall("(?<![\^\-])[\-\d\.]+",input2):
+			num = re.findall("(?<![\^\-])[\-\d\.]+",input2)[0]
+			num = float(num)
+			coef = coef / num #WHEN NO COEFF THEN MULTIPLIES EXP...
+		a = "%s" %coef
+		b = "%s" %var1
+		c = "^%s" %exp
+		answer = a+b+c
+	#####HOW DO YOU HANDEL -x WITHOUT A COEFF???
+	else: 
+		anwer = False #"False"
+	return answer
+
 #variable Addition
 def varAdd(input1,input2):
 	print("Variable Addition:")
@@ -78,19 +118,39 @@ def varAdd(input1,input2):
 
 	return answer
 
+#variable Addition
+def varSub(input1,input2):
+	print("Variable Subtraction:")
+	coef = 0
+	var1 = re.findall("([a-z])",input1)[0]
+	var2 = re.findall("([a-z])",input2)[0]
+	exp1 = re.findall("\^(\-?\d+)",input1)[0]
+	exp2 = re.findall("\^(\-?\d+)",input2)[0]
+	if var1 == var2 and exp1 == exp2:# and if EXPONENTS ARE THE SAME:
+		#coeffients
+		if re.findall("(?<![\^\-])[\-\d\.]+",input1):
+			num = re.findall("(?<![\^\-])[\-\d\.]+",input1)[0]
+			num = float(num)
+			coef = coef + num 
+		if re.findall("(?<![\^\-])[\-\d\.]+",input2):
+			num = re.findall("(?<![\^\-])[\-\d\.]+",input2)[0]
+			num = float(num)
+			coef = coef - num
+		a = "%s" %coef
+		b = "%s" %var1
+		c = "^%s" %exp1
+		answer = a+b+c
+
+	else:
+		anwer = False #"False"
+
+	return answer
+
 
 #inputs
-debug = True
-
-if debug:
-	operation = "x*x*x=16"
-else:
-	operation = input("What calculation would you like to complete:")
-
-a = '4x^3'
-b = '2x^3'
-print(varMult(a,b))
+a = '4x^5'
+b = '2x^5'
+print(varMul(a,b))
+print(varDiv(a,b))
 print(varAdd(a,b))
-
-#REGEX
-#distribution: \d*[a-z]*\(.+[\+\-].+\)
+print(varSub(a,b))
