1960c9cf5e1fbced121e65f2fc19be73875da191
Sophia <sbaker@putneyschool.org>
2018-02-15 09:43:51 -0500

    + and -x

--- Calculator_x_Regex.py
+++ Calculator_x_Regex.py
@@ -10,17 +10,19 @@ import re
 #variable multiplicaiton
 def varMul(input1,input2):
 	print("Variable Multiplicaiton:")
+
 	exp = 0
 	coef = 1
 	var1 = re.findall("([a-z])",input1)[0]
-	print(var1)
 	var2 = re.findall("([a-z])",input2)[0]
+
 	#replace -x with -1x
-	if re.findall("(\-[a-z])",input1) or re.findall("(\-[a-z])",input2):
+	if re.findall("(\-[a-z])",input1):
 		input1 = input1.replace(str(re.findall("(\-[a-z])",input1)[0]), "-1%s"%var1)
+	if re.findall("(\-[a-z])",input2):
 		input2 = input2.replace(str(re.findall("(\-[a-z])",input2)[0]), "-1%s"%var2)
+
 	if var1 == var2:
-		print(input1)
 		#num of variable occerences
 		exp1 = input1.count(var1)
 		exp2 = input2.count(var2)
@@ -29,7 +31,7 @@ def varMul(input1,input2):
 		if re.findall("\^(\-?\d+)",input1):
 			num = re.findall("\^(\-?\d+)",input1)[0]
 			num = float(num)
-			exp = exp + num -1 #-1 sothat we dont acount for an extera variable from count above
+			exp = exp + num -1 #-1 so that we dont acount for an extera variable from count above
 		if re.findall("\^(\-?\d+)",input2):
 			num = re.findall("\^(\-?\d+)",input2)[0]
 			num = float(num)
@@ -56,24 +58,27 @@ def varMul(input1,input2):
 #variable Division
 def varDiv(input1,input2):
 	print("Variable Division:")
+
 	exp = 0
 	coef = 1
 	var1 = re.findall("([a-z])",input1)[0]
 	var2 = re.findall("([a-z])",input2)[0]
-		#replace -x with -1x
-	if re.findall("(\-[a-z])",input1) or re.findall("(\-[a-z])",input2):
+
+	#replace -x with -1x
+	if re.findall("(\-[a-z])",input1):
 		input1 = input1.replace(str(re.findall("(\-[a-z])",input1)[0]), "-1%s"%var1)
+	if re.findall("(\-[a-z])",input2):
 		input2 = input2.replace(str(re.findall("(\-[a-z])",input2)[0]), "-1%s"%var2)
+
 	if var1 == var2:
 		#num of variable occerences
 		exp1 = input1.count(var1)
 		exp2 = input2.count(var2)
-		#exp = exp1 - exp2
 		#exponents
 		if re.findall("\^(\-?\d+)",input1):
 			num = re.findall("\^(\-?\d+)",input1)[0]
 			num = float(num)
-			expA = exp1 + num -1 #-1 sothat we dont acount for an extera variable from count above
+			expA = exp1 + num -1 #-1 so that we dont acount for an extera variable from count above
 		if re.findall("\^(\-?\d+)",input2):
 			num = re.findall("\^(\-?\d+)",input2)[0]
 			num = float(num)
@@ -88,6 +93,7 @@ def varDiv(input1,input2):
 			num = re.findall("(?<![\^\-])[\-\d\.]+",input2)[0]
 			num = float(num)
 			coef = coef / num #WHEN NO COEFF THEN MULTIPLIES EXP...
+			#print(coef)
 		a = "%s" %coef
 		b = "%s" %var1
 		c = "^%s" %exp
@@ -100,15 +106,19 @@ def varDiv(input1,input2):
 #variable Addition
 def varAdd(input1,input2):
 	print("Variable Addition:")
+
 	coef = 0
 	var1 = re.findall("([a-z])",input1)[0]
 	var2 = re.findall("([a-z])",input2)[0]
 	exp1 = re.findall("\^(\-?\d+)",input1)[0]
 	exp2 = re.findall("\^(\-?\d+)",input2)[0]
-		#replace -x with -1x
-	if re.findall("(\-[a-z])",input1) or re.findall("(\-[a-z])",input2):
+
+	#replace -x with -1x
+	if re.findall("(\-[a-z])",input1):
 		input1 = input1.replace(str(re.findall("(\-[a-z])",input1)[0]), "-1%s"%var1)
+	if re.findall("(\-[a-z])",input2):
 		input2 = input2.replace(str(re.findall("(\-[a-z])",input2)[0]), "-1%s"%var2)
+
 	if var1 == var2 and exp1 == exp2:
 		#coeffients
 		if re.findall("(?<![\^\-])[\-\d\.]+",input1):
@@ -132,15 +142,19 @@ def varAdd(input1,input2):
 #variable Addition
 def varSub(input1,input2):
 	print("Variable Subtraction:")
+
 	coef = 0
 	var1 = re.findall("([a-z])",input1)[0]
 	var2 = re.findall("([a-z])",input2)[0]
 	exp1 = re.findall("\^(\-?\d+)",input1)[0]
 	exp2 = re.findall("\^(\-?\d+)",input2)[0]
-		#replace -x with -1x
-	if re.findall("(\-[a-z])",input1) or re.findall("(\-[a-z])",input2):
+
+	#replace -x with -1x
+	if re.findall("(\-[a-z])",input1):
 		input1 = input1.replace(str(re.findall("(\-[a-z])",input1)[0]), "-1%s"%var1)
+	if re.findall("(\-[a-z])",input2):
 		input2 = input2.replace(str(re.findall("(\-[a-z])",input2)[0]), "-1%s"%var2)
+
 	if var1 == var2 and exp1 == exp2:
 		#coeffients
 		if re.findall("(?<![\^\-])[\-\d\.]+",input1):
@@ -163,8 +177,8 @@ def varSub(input1,input2):
 
 
 #inputs
-a = '-x^3'
-b = '-x^3'
+a = '-x^4'
+b = '5x^4'
 print(varMul(a,b))
 print(varDiv(a,b))
 print(varAdd(a,b))
