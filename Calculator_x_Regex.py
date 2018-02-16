41d8ac0b8d8494d8a2a592fb3e8e6170400b0fc0
Sophia <sbaker@putneyschool.org>
2018-02-16 08:54:24 -0500

    added in regular operations - combined code

--- Calculator_x_Regex.py
+++ Calculator_x_Regex.py
@@ -5,6 +5,92 @@
 #Regex
 import re
 
+#################################################################################################################
+
+#Exponents
+def exp(string):
+	print("Exponents:")
+	while '^' in string:
+		var1 = re.findall("(\(?\d+\.?\d*\)?)\^",string)[0]
+		var2 = re.findall("\^(\(?\d+\.?\d*\)?)",string)[0]
+
+		#print("var1 = " + var1)
+		#print("var2 = " + var2)
+
+		answer = float(var1) ** float(var2)
+		#print("answer = " + str(answer))
+		
+		string = string.replace(var1 + "^" + var2, str(answer))
+
+		#print("string = " + string)
+
+	print("string = " + string)
+
+	return string
+
+#Multiplication and Division
+def md(string):
+	print("Multiplication & Division:")
+	while '*' in string or '/' in string:
+		var1 = re.findall("(\-?\(?\d+\.?\d*\)?)[\*\/]",string)[0]
+		var2 = re.findall("[\*\/](\-?\(?\d+\.?\d*\)?)",string)[0]
+		operator = re.findall("[\*\/]",string)[0]
+
+		#print("var1 = " + var1)
+		#print("var2 = " + var2)
+		#print("operation: " + operator)
+
+		if operator == '*':
+			answer = float(var1) * float(var2)
+			#print("answer = " + str(answer))
+		elif operator == '/': 
+			answer = float(var1)/float(var2)
+			#print("answer = " + str(answer))
+		
+		string = string.replace(var1 + operator + var2, str(answer))
+		#print("string = " + string)
+
+	print("string = " + string)
+
+	return string
+
+#Addition and Subtraction
+def sumAll(string):
+	print("Addition & Subtraction:")
+	if '+' in string or '-' in string:
+		for i in string:
+			if i == '+' in string or i == '-' in string:
+				line = re.findall("(\-?\d\.?\d*e\+\d+|\-?\(?\d+\.?\d*\)?)", string)
+				print("List: " + str(line))
+				answer = sum(float(x) for x in line)
+				print("answer = " + str(answer))
+				break
+	else:
+		print("no Multiplication or Division")
+		answer = string
+	
+	return answer
+
+#Parenthisis
+def parenth(string):
+	print("Parenthisis:")
+	while '(' in string or ')' in string:
+		part = re.findall("\(([^\)]+)\)",string)[0]
+		print("part: " + part)
+
+		
+		answer = parenth(part)
+		answer = exp(answer)
+		answer = md(answer)
+		answer = sumAll(answer)
+		
+		string = string.replace("(" + part + ")", str(answer))
+
+	print("string = " + string)
+
+	return string
+
+#################################################################################################################
 
 #variables
 #variable multiplicaiton
@@ -173,6 +259,25 @@ def varSub(input1,input2):
 
 	return answer
 
+#################################################################################################################
+
+print("welcome to calculator.py")
+
+#inputs
+debug = True
+
+if debug:
+	operation = "1.4e+6+15-3"
+else:
+	operation = input("What calculation would you like to complete:")
+
+#performing calulations in order of operation:	
+operation = parenth(operation)
+operation = exp(operation)
+operation = md(operation)
+operation = sumAll(operation)
+
+print("the answer to your calculation is: " + str(operation))
 
 #inputs
 a = '-x^4'
@@ -181,3 +286,4 @@ print(varMul(a,b))
 print(varDiv(a,b))
 print(varAdd(a,b))
 print(varSub(a,b))
+
