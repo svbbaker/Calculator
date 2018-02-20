a9aef51075f2424f6bfd18804c938989f7f9a57e
Sophia <sbaker@putneyschool.org>
2018-02-20 09:58:12 -0500

    seperatied out multiplicaiton

--- Calculator_x_Regex.py
+++ Calculator_x_Regex.py
@@ -9,22 +9,13 @@ import re
 
 #Exponents
 def expo(string):
-	print("Exponents:")
+	#print("Exponents:")
 	while '^' in string:
-		var1 = re.findall("(\(?\d+\.?\d*\)?)\^",string)[0] #^[\e\+\d+]
+		var1 = re.findall("(\(?\d+\.?\d*\)?)\^",string)[0]
 		var2 = re.findall("\^(\(?\d+\.?\d*\)?)",string)[0]
-
-		print("var1 = " + var1)
-		print("var2 = " + var2)
-
 		answer = float(var1) ** float(var2)
-		#print("answer = " + str(answer))
-		
+
 		string = string.replace(var1 + "^" + var2, str(answer))
-
-		#print("string = " + string)
-
-	print("string = " + string)
 
 	return string
 
@@ -36,21 +27,12 @@ def md(string):
 		var2 = re.findall("[\*\/](\-?\(?\d+\.?\d*\)?)",string)[0]
 		operator = re.findall("[\*\/]",string)[0]
 
-		#print("var1 = " + var1)
-		#print("var2 = " + var2)
-		#print("operation: " + operator)
-
 		if operator == '*':
 			answer = float(var1) * float(var2)
-			#print("answer = " + str(answer))
 		elif operator == '/': 
 			answer = float(var1)/float(var2)
-			#print("answer = " + str(answer))
 		
 		string = string.replace(var1 + operator + var2, str(answer))
-		#print("string = " + string)
-
-	#print("string = " + string)
 
 	return string
 
@@ -63,10 +45,8 @@ def sumAll(string):
 				line = re.findall("(\-?\d\.?\d*e\+\d+|\-?\(?\d+\.?\d*\)?)", string)
 				#print("List: " + str(line))
 				answer = sum(float(x) for x in line)
-				#äprint("answer = " + str(answer))
 				break
 	else:
-		#print("no Multiplication or Division")
 		answer = string
 	
 	return answer
@@ -77,7 +57,6 @@ def parenth(string):
 	while '(' in string or ')' in string:
 		part = re.findall("\(([^\)]+)\)",string)[0]
 		print("part: " + part)
-
 		
 		answer = parenth(part)
 		answer = expo(answer)
@@ -86,8 +65,6 @@ def parenth(string):
 		
 		string = string.replace("(" + part + ")", str(answer))
 
-	#print("string = " + string)
-
 	return string
 
 #################################################################################################################
@@ -95,7 +72,7 @@ def parenth(string):
 #variables
 #variable multiplicaiton
 def varMul(input1,input2):
-	print("Variable Multiplicaiton:")
+	#print("Variable Multiplicaiton:")
 
 	exp = 0
 	coef = 1
@@ -143,13 +120,15 @@ def varMul(input1,input2):
 		answer = a+b+c
 
 	else:
-		answer = False	
+		a = "%s" %input1
+		b = "%s" %input2
+		answer = a + "*" + b
 
 	return answer
 
 #variable multiplicaiton
 def varMulComb(input1,input2):
-	print("Variable Combined Multiplicaiton:")
+	#print("Variable Combined Multiplicaiton:")
 
 	exp = 0
 	coef = 1
@@ -203,13 +182,15 @@ def varMulComb(input1,input2):
 		answer = a+b+c
 
 	else:
-		answer = False	
+		a = "%s" %input1
+		b = "%s" %input2
+		answer = a + "*" + b
 
 	return answer
 
 #variable Division
 def varDiv(input1,input2):
-	print("Variable Division:")
+	#print("Variable Division:")
 
 	exp = 0
 	coef = 1
@@ -256,12 +237,15 @@ def varDiv(input1,input2):
 		c = "^%s" %exp
 		answer = a+b+c
 	else: 
-		answer = False #"False"
+		a = "%s" %input1
+		b = "%s" %input2
+		answer = a + "*" + b
+
 	return answer
 
 #variable Addition
 def varAdd(input1,input2):
-	print("Variable Addition:")
+	#print("Variable Addition:")
 
 	coef = 0
 	
@@ -300,13 +284,15 @@ def varAdd(input1,input2):
 		answer = a+b+c
 
 	else:
-		answer = False
+		a = "%s" %input1
+		b = "%s" %input2
+		answer = a + "*" + b
 
 	return answer
 
 #variable Addition
 def varSub(input1,input2):
-	print("Variable Subtraction:")
+	#print("Variable Subtraction:")
 
 	coef = 0
 	
@@ -345,9 +331,48 @@ def varSub(input1,input2):
 		answer = a+b+c
 
 	else:
-		answer = False
+		a = "%s" %input1
+		b = "%s" %input2
+		answer = a + "*" + b
 
 	return answer
+
+#################################################################################################################
+#COMBINED
+
+#Exponents
+# HOW TO DEAL WITH 4^x * BLAH ?!??!?!?!?!?!?! here
+
+#Multiplicaiton
+def multiplication(string):
+	while "*" in string:
+		if re.findall("(\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)",string): #if variables
+			var1 = re.findall("(\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)[\*\/]",string)[0]
+			var2 = re.findall("[\*\/](\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)",string)[0]
+			varA = expo(var1)
+			varB = expo(var2)
+			part = varMul(varA,varB)
+			string = string.replace(var1 + "*" + var2, str(part))
+			print(string)
+		if re.findall("(\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*)|(\-?\d+\.?\d*\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)",string): #if mixed variables
+			var1 = re.findall("(\-?\d+\.?\d*\^?\d*[a-z]?\^?\d*)[\*\/]",string)[0]
+			var2 = re.findall("[\*\/](\-?\d+\.?\d*\^?\d*[a-z]?\^?\d*)",string)[0]
+			varA = expo(var1)
+			varB = expo(var2)
+			part = varMulComb(varA,varB)
+			string = string.replace(var1 + "*" + var2, str(part))
+			print(string)
+		if re.findall("(\-?\d+\.?\d*\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*)",string): #if variables 
+			var1 = re.findall("(\-?\d+\.?\d*\^?\d*)[\*\/]",string)[0]
+			var2 = re.findall("[\*\/](\-?\d+\.?\d*\^?\d*)",string)[0]
+			varA = expo(var1)
+			varB = expo(var2)
+			part = varA + "*" + varB
+			part = md(part)
+			string = string.replace(var1 + "*" + var2, str(part))
+			print(string)
+	return string
+
 
 #################################################################################################################
 
@@ -355,46 +380,11 @@ def distrib(string):
 	print("Distributive Property:")
 	if re.findall("((\-?\d+?[a-z]|\-?\d)\((.+)\))",string):
 		inside = re.findall("\((.+)\)",string)[0]
-		if re.findall("(\-?\d+\.?\d*\^?\d*[a-z]?\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*[a-z]?\^?\d*)",inside): #if *
-			if re.findall("(\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)",inside): #if variables
-				var1 = re.findall("(\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)[\*\/]",inside)[0]
-				var2 = re.findall("[\*\/](\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)",inside)[0]
-				varA = expo(var1)
-				varB = expo(var2)
-				# HOW TO DEAL WITH 4^X * BLAH ?!??!?!?!?!?!?! here
-				part = varMul(varA,varB)
-				inside = inside.replace(var1 + "*" + var2, str(part))
-				#print("new inside:")
-				#print(inside)
-			if re.findall("(\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*)|(\-?\d+\.?\d*\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)",inside): #if mixed variables
-				#print("part:")
-				#print(re.findall("(\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*[a-z]\^?\d*)",inside)) 
-				var1 = re.findall("(\-?\d+\.?\d*\^?\d*[a-z]?\^?\d*)[\*\/]",inside)[0]
-				#print(var1)
-				var2 = re.findall("[\*\/](\-?\d+\.?\d*\^?\d*[a-z]?\^?\d*)",inside)[0]
-				#print(var2)
-				varA = expo(var1)
-				varB = expo(var2)
-				# HOW TO DEAL WITH 4^x * BLAH ?!??!?!?!?!?!?! here
-				part = varMulComb(varA,varB)
-				#print(part)
-				inside = inside.replace(var1 + "*" + var2, str(part))
-				print("new inside:")
-				print(inside)
-			#print("inside:")
-			#print(re.findall("(\-?\d+\.?\d*\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*)",inside))
-			if re.findall("(\-?\d+\.?\d*\^?\d*)[\*\/](\-?\d+\.?\d*\^?\d*)",inside): #if variables 
-				var1 = re.findall("(\-?\d+\.?\d*\^?\d*)[\*\/]",inside)[0]
-				var2 = re.findall("[\*\/](\-?\d+\.?\d*\^?\d*)",inside)[0]
-				varA = expo(var1)
-				varB = expo(var2)
-				part = varA + "*" + varB
-				part = md(part)
-				inside = inside.replace(var1 + "*" + var2, str(part))
-				return inside
-		answer = True
-		print(answer)
-	return answer
+		#PARENTHISIS
+		inside = parenthisis(inside)
+		inside = multiplication(inside)
+		print("string = " + inside)
+return string
 
 
 #################################################################################################################
@@ -417,7 +407,8 @@ else:
 #print("the answer to your calculation is: " + str(operation))
 
 function = '5(5^2x*2x+2*3^2+2x^2*5^2)'
-#function = distrib(function)
+print(function)
+function = distrib(function)
 
 #inputs
 a = '2^2'
@@ -429,7 +420,7 @@ b = '5'
 
 x = "x^2"
 y = "3^2"
-varMulComb(x,y)
+#varMulComb(x,y)
 #print(y)
 #expo(y)
 
